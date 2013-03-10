124d638476e04bcd8e8a7be97fbc7bef777048c9
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-03-09 19:04:33 -0500

    ..

--- CDVGeoCoder.xctemplate/___FILEBASENAME___ViewController.m
+++ CDVGeoCoder.xctemplate/___FILEBASENAME___ViewController.m
@@ -15,7 +15,7 @@
 @synthesize delegate, orientationDelegate;
 @synthesize spinner, webView, addressLabel;
 @synthesize closeBtn, refreshBtn, backBtn, fwdBtn, safariBtn;
-@synthesize mapView,latLngLabel;
+@synthesize mapView,latLngLabel,searchBar;
 
 /*
  *   // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
